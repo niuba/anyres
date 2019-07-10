8cb791b3f12b88592de822420131d406ad3f6c75
niuba <niuba.zbz@gmail.com>
2019-07-10 17:48:11 +0800

    change name
    Signed-off-by: niuba <niuba.zbz@gmail.com>

--- packages/core-cache/src/decorators/cache.decorator.ts
+++ packages/core-cache/src/decorators/cache.decorator.ts
@@ -1,4 +1,11 @@
-import { AnyresCRUD, IResCreate, IResGet, IResQuery, IResQueryResult, IResUpdate } from "@anyres/core";
+import {
+  AnyresCRUD,
+  IResCreate,
+  IResGet,
+  IResQuery,
+  IResQueryResult,
+  IResUpdate,
+} from "@anyres/core";
 import { ICacheParams } from "../interfaces";
 
 export function Cache({ store, expire = 0 }: ICacheParams) {
@@ -9,9 +16,11 @@ export function Cache({ store, expire = 0 }: ICacheParams) {
     TC extends IResCreate,
     TU extends IResUpdate,
     T extends {
-      new(...args: any[]): AnyresCRUD<TQ, TQR, TG, TC, TU>,
+      new (...args: any[]): AnyresCRUD<TQ, TQR, TG, TC, TU>;
     }
-  >(target: T) => {
+  >(
+    target: T,
+  ) => {
     return class CacheAnyresCRUD extends target {
       public get(id: number | string) {
         const key = `${target.prototype.path}/${id}`;
